9fa1e2996567497f0e2333284a9798b235e3a5fd
gabrielsbf <gabrielsbfernandes@gmail.com>
2025-03-26 14:53:20 -0300

    feat: Tiktok going through links

--- PlayWrightAuto/SocialMedia/TikTok.py
+++ PlayWrightAuto/SocialMedia/TikTok.py
@@ -4,29 +4,41 @@ class Tiktok_Automation(PlayEssencial):
     def __init__(self):
         super().__init__("https://tiktok.com/@niteroipref")
 
+
+    def iterate_video_links(self, result_info: dict):
+        for key, _ in result_info.items():
+            yield key
+
+    def access_videos(self, result_info: dict):
+        for link in self.iterate_video_links(result_info):
+            self.current_url = link
+            self.page.goto(self.current_url, timeout=30000)
+            # dados = self.page.evaluate('''() => {
+            #     if (window.webapp && window.webapp.videoDetail && window.webapp.videoDetail.itemInfo) {
+            #         const itemInfo = window.webapp.videoDetail.itemInfo.itemStruct;
+            #         return {
+            #             id: itemInfo.id,
+            #             desc: itemInfo.desc,
+            #             createTime: itemInfo.createTime
+            #         };
+            #     }
+            #     return null;
+            # }''')
+            self.page.on()
+            print("DADOS IS: ", dados)
+
     def get_feed_info(self):
             result_info = {}
             if not self.page:
                 raise Exception("Browser or page not initialized. Call start_browser() first.")
             self.page.goto(self.current_url, timeout=30000)
             input()
-            dados = self.page.evaluate('''() => {
-            if (window.webapp && window.webapp.videoDetail && window.webapp.videoDetail.itemInfo) {
-                const itemInfo = window.webapp.videoDetail.itemInfo.itemStruct;
-                return {
-                    id: itemInfo.id,
-                    desc: itemInfo.desc,
-                    createTime: itemInfo.createTime
-                };
-            }
-            return null;
-        }''')
+ 
       
             self.page.wait_for_load_state("domcontentloaded",timeout=30000)
 
             self.page.wait_for_selector("//div[@id='main-content-others_homepage']")
             feed = self.page.locator("//div[@id='main-content-others_homepage']")
-            
             items = feed.locator('//div[@class="css-1uqux2o-DivItemContainerV2 e19c29qe7"]')
             views = self.page.locator('//div[@class="css-1qb12g8-DivThreeColumnContainer eegew6e2"]//strong[@class="video-count css-dirst9-StrongVideoCount e148ts222"]')
             count = items.count()
@@ -38,5 +50,6 @@ class Tiktok_Automation(PlayEssencial):
     def standard_procedure(self):
          self.start_browser()
          data = self.get_feed_info()
+         self.access_videos(data)
          self.stop_browser()
          print(data)
