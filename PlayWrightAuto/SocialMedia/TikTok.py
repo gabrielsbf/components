fedfabaa32e3e488d3ec44bc963f6b1e30f4445e
Zeyou2 <alex_100894@hotmail.com>
2025-06-11 12:01:19 -0300

    feat: add new general feature

--- PlayWrightAuto/SocialMedia/TikTok.py
+++ PlayWrightAuto/SocialMedia/TikTok.py
@@ -39,7 +39,7 @@ class Tiktok_Automation(PlayEssencial):
 		result = re.findall(r'\d+', result)
 		return result
 
-	def get_request_createdTime(self, response, result_info: dict, start_date, end_date):
+	def get_request_createdTime(self, response, result_info: dict, start_date, end_date, photo_metrics = None):
 		def requests_seletion(response, find_term, configs : str):
 			begin = response.text.find(find_term)
 			end = response.text[begin:].find(configs) + begin
@@ -51,7 +51,19 @@ class Tiktok_Automation(PlayEssencial):
 		findResp = response.text.find("webapp.video-detail") - 1
 		if findResp <= -1:
 			print("not found")
-			result_info[self.current_url]["date_created"] = "notFound"
+			result_info[self.current_url]["date_created"] = photo_metrics[4] if photo_metrics else "notFound"
+			result_info[self.current_url]["curtidas"] = photo_metrics[0] if photo_metrics else "notFound"
+			result_info[self.current_url]["compartilhamentos"] = photo_metrics[3] if photo_metrics else "notFound"
+			result_info[self.current_url]["comentários"] = photo_metrics[1] if photo_metrics else "notFound"
+			result_info[self.current_url]["reproduções"] = photo_metrics[5] if photo_metrics else "notFound"
+			result_info[self.current_url]["salvos"] = photo_metrics[2] if photo_metrics else "notFound"
+			result_info[self.current_url]["repostado"] = photo_metrics[6] if photo_metrics else "notFound"
+			if photo_metrics != None:
+				processed_date = datetime.datetime.strptime(photo_metrics[4], '%d/%m/%Y %H:%M:%S')
+				if processed_date < start_date:
+					return (0)
+				if processed_date > end_date:
+					return (1)
 		else:
 			begin = response.text[findResp:].find("createTime") + findResp - 1
 			end = response.text[begin:].find(",") + begin
@@ -64,13 +76,13 @@ class Tiktok_Automation(PlayEssencial):
 			if processed_date > end_date:
 				return (1)
 			result_info[self.current_url]["date_created"] = datetime.datetime.fromtimestamp(int(result)).strftime("%d/%m/%Y %H:%M:%S")
-		interactions = requests_seletion(response, '"statsV2":', '}')
-		result_info[self.current_url]["curtidas"] = interactions[0] if len(interactions) != 0 else "notFound"
-		result_info[self.current_url]["compartilhamentos"] = interactions[1] if len(interactions) != 0 else "notFound"
-		result_info[self.current_url]["comentários"] = interactions[2] if len(interactions) != 0 else "notFound"
-		result_info[self.current_url]["reproduções"] = interactions[3] if len(interactions) != 0 else "notFound"
-		result_info[self.current_url]["salvos"] = interactions[4] if len(interactions) != 0 else "notFound"
-		result_info[self.current_url]["repostado"] = interactions[5] if len(interactions) != 0 else "notFound"
+			interactions = requests_seletion(response, '"statsV2":', '}')
+			result_info[self.current_url]["curtidas"] = interactions[0] if len(interactions) != 0 else "notFound"
+			result_info[self.current_url]["compartilhamentos"] = interactions[1] if len(interactions) != 0 else "notFound"
+			result_info[self.current_url]["comentários"] = interactions[2] if len(interactions) != 0 else "notFound"
+			result_info[self.current_url]["reproduções"] = interactions[3] if len(interactions) != 0 else "notFound"
+			result_info[self.current_url]["salvos"] = interactions[4] if len(interactions) != 0 else "notFound"
+			result_info[self.current_url]["repostado"] = interactions[5] if len(interactions) != 0 else "notFound"
 		return(self.current_url)
 		
 
@@ -79,20 +91,37 @@ class Tiktok_Automation(PlayEssencial):
 		all_videos = []
 		counter = 0
 		for link in self.iterate_video_links(result_info):
-			print('entrei')
 			self.set_url(link)
+			
+			print('entrei')
 			link_splited = link.split('/')
-			print("link_splited is: ", link_splited)
-			# self.page.goto(self.current_url, timeout=30000)
-			# self.page.wait_for_load_state("domcontentloaded",timeout=30000)
-			# self.page.wait_for_selector("//button[@class='css-1ncfmqs-ButtonActionItem e1hk3hf90']")
-			# mertrics = self.page.locator("//button[@class='css-1ncfmqs-ButtonActionItem e1hk3hf90']//strong[@class='css-1w013xe-StrongText e1hk3hf92']").all_inner_texts()
-			# print("mertrics are: ", mertrics)
+			def search_metrics_photo(link_splited):
+				if link_splited[4] == "photo":
+					print("This is a photo")
+					self.page.goto(self.current_url, timeout=30000)
+					self.page.wait_for_load_state("domcontentloaded",timeout=30000)
+					input("VERIFY IF THE PAGE HAS A PROBLEM OF CAPTCHA OR ERROR. THEN, PRESS ENTER TO CONTINUE")
+					self.page.wait_for_selector("//button[@class='css-rninf8-ButtonActionItem edu4zum0']")
+					mertrics = self.page.locator("//button[@class='css-rninf8-ButtonActionItem edu4zum0']//strong[@class='css-1l70c6-StrongText edu4zum2']").all_inner_texts()
+					date = self.page.locator("//span[@class='css-5set0y-SpanOtherInfos evv7pft3']").all_inner_texts()
+					print("mertrics are: ", mertrics)
+					print("date is: ", date)
+					data_str = re.findall(r'\d+', date[0])
+					print("data_str is: ", data_str)
+					if len(data_str) < 3:
+						year_now = datetime.datetime.now().year
+						print("year_now is: ", year_now)
+						data_str.insert(0, str(year_now))
+					processed_date = datetime.datetime.strptime("/".join(data_str), '%Y/%m/%d')
+					mertrics.append(processed_date.strftime("%d/%m/%Y %H:%M:%S"))
+					return mertrics
 			response = requests.get(self.current_url, headers=self.headers)
-			element_vid = self.get_request_createdTime(response, result_info, start_date, end_date)
-			element = self.requests_seletion(response, '"statsV2":', '}')
-			print("elements is >>>", element)
+			metrics_photo = search_metrics_photo(link_splited)
+			element_vid = self.get_request_createdTime(response, result_info, start_date, end_date, metrics_photo)
+			# element = self.requests_seletion(response, '"statsV2":', '}')
+			# print("elements is >>>", element)
 			print("ALL VIDEOS ARE: ", all_videos)
+			print("element_vid is: ", element_vid)
 			if (element_vid != 0 and element_vid != 1):
 				all_videos.append(element_vid)
 			if (element_vid == 0 and counter > 3):
@@ -113,7 +142,6 @@ class Tiktok_Automation(PlayEssencial):
 			items = feed.locator('//div[@class="css-1uqux2o-DivItemContainerV2 e19c29qe7"]')
 			views = self.page.locator('//div[@class="css-1qb12g8-DivThreeColumnContainer eegew6e2"]//strong[@class="video-count css-dirst9-StrongVideoCount e148ts222"]')
 			count = items.count()
-			input()
 			for i in range(count):
 				item = items.nth(i)
 				result_info[item.locator("a").get_attribute("href")] = {"description": item.locator("img").get_attribute("alt"), 'views' : views.nth(i).inner_text()}
