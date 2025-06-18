9899b47fb291cc88f24bb0024a3c448e87f6dae8
Zeyou2 <alex_100894@hotmail.com>
2025-06-18 16:23:02 -0300

    feat: add logger debug

--- PlayWrightAuto/SocialMedia/Twitter.py
+++ PlayWrightAuto/SocialMedia/Twitter.py
@@ -1,7 +1,15 @@
 from components.PlayWrightAuto.essencial import PlayEssencial
-from datetime import datetime, timezone
+from datetime import datetime
+import logging
 import re
 
+
+
+logging.basicConfig(
+    level=logging.DEBUG,  
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 class Twitter_Automation(PlayEssencial):
     def __init__(self, account, playwright=None, browser_data_path=None, chrome_executable_path=None, browser=None, page=None):
@@ -32,6 +40,16 @@ class Twitter_Automation(PlayEssencial):
                 A list of dictionaries where each item has the post URL as the key, 
                 and the associated metadata as the value.
         """
+        def convert_text_to_metrics(metrics_text : str)-> dict:
+                pattern = r"(\d+)\s+(respostas?|repost|curtidas?|visualizações)"
+                matches = re.findall(pattern, metrics_text)
+                metrics_dict = {}
+                for value, key in matches:
+                    if not key.endswith('s'):
+                        key += 's'
+                    key = key.lower()
+                    metrics_dict[key] = int(value)
+                return metrics_dict
         if not self.page:
             raise Exception("Browser or page not initialized. Call start_browser() first.")
         self.set_url(self.current_url)
@@ -58,30 +76,16 @@ class Twitter_Automation(PlayEssencial):
                 post.locator("//div[@class='css-175oi2r r-1kbdv8c r-18u37iz r-1wtj0ep r-1ye8kvj r-1s2bzr4']").first.wait_for(state="visible", timeout=5000)
                 engagement_summary_str = post.locator("//div[@class='css-175oi2r r-1kbdv8c r-18u37iz r-1wtj0ep r-1ye8kvj r-1s2bzr4']").get_attribute("aria-label")
 
-                datetime_element = post.locator('a:has(time)').first
-                datetime_element.wait_for(state="visible", timeout=5000)
-
-                datetime_str = datetime_element.locator("time").get_attribute("datetime")
+                element = post.locator('a:has(time)').first
+                element.wait_for(state="visible", timeout=5000)
+                datetime_str = element.locator("time").get_attribute("datetime")
                 post_datetime = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%fZ")
 
-                post_url = datetime_element.get_attribute("href")
+                post_url = element.get_attribute("href")
 
                 if post_url in processed_hrefs:
                     continue
-                processed_hrefs.add(post_url
-                                    )
-                def convert_text_to_metrics(metrics_text : str)-> dict:
-                    pattern = r"(\d+)\s+(respostas?|repost|curtidas?|visualizações)"
-                    matches = re.findall(pattern, metrics_text)
-                    metrics_dict = {}
-                    for value, key in matches:
-                        if not key.endswith('s'):
-                            key += 's'
-                        key = key.lower()
-                        metrics_dict[key] = value
-                    return metrics_dict
-                
-
+                processed_hrefs.add(post_url)          
                 post_metrics = convert_text_to_metrics(engagement_summary_str)
                 post_decription = post.locator('[data-testid="tweetText"]').inner_text()
                 post_decription = re.sub(r'\s+', ' ', post_decription).strip()
@@ -93,26 +97,25 @@ class Twitter_Automation(PlayEssencial):
                 if post_datetime >  end_date:
                     continue
 
-                print(f"Data do post: {post_datetime}")
-                print(f"Link encontrado: {post_url}")
-                print(f"Descrição do post: {post_decription}")
-                print(f"Métricas do post: {engagement_summary_str}")
-                print(f"Métricas convertidas: {post_metrics}")
+                logger.debug(f"Data do post: {post_datetime}")
+                logger.debug(f"Link encontrado: {post_url}")
+                logger.debug(f"Descrição do post: {post_decription}")
+                logger.debug(f"Métricas do post: {engagement_summary_str}")
+                logger.debug(f"Métricas convertidas: {post_metrics}")
 
-                if start_date <= post_datetime <= end_date:
-                    filtered_posts.append(({f"https://www.x.com{post_url}" : 
-                                                {'Descrição' : post_decription, 
-                                                'Data' : post_datetime, 
-                                                'Comentários' : post_metrics.get('respostas', '0'), 
-                                                'Compartilhamentos' : post_metrics.get('reposts', '0'),
-                                                'Curtidas' :  post_metrics.get('curtidas', '0'),
-                                                'Visualizações' :  post_metrics.get('visualizações', '0') }}))
-                    print(f"Link adicionado: {post_url}")
-        print("Links filtrados:", filtered_posts)
-        print(f"Total de links filtrados: {len(filtered_posts)}")
+                filtered_posts.append(({f"https://www.x.com{post_url}" : 
+                                            {'Descrição' : post_decription, 
+                                            'Data' : post_datetime, 
+                                            'Comentários' : post_metrics.get('respostas', 0), 
+                                            'Compartilhamentos' : post_metrics.get('reposts', 0),
+                                            'Curtidas' :  post_metrics.get('curtidas', 0),
+                                            'Visualizações' :  post_metrics.get('visualizações', 0) }}))
+                logger.debug(f"Link adicionado: {post_url}")
+        logger.debug(f"Links filtrados: {filtered_posts}")
+        logger.debug(f"Total de links filtrados: {len(filtered_posts)}")
         return filtered_posts
 
-    def standard_procedure(self, dates: list[datetime]):
+    def standard_procedure(self, dates: list[datetime])-> list[dict]:
         self.start_browser_user()
         data = self.collect_filtered_post_links(dates[0], dates[1])
         self.stop_browser()
