b0bdfc44ef5ee8385ba3f0792fe1601b032e2ae6
Zeyou2 <alex_100894@hotmail.com>
2025-06-18 14:35:08 -0300

    feat: add post collection with text cleaning, standardized metrics and duplicate removal

--- PlayWrightAuto/SocialMedia/Twitter.py
+++ PlayWrightAuto/SocialMedia/Twitter.py
@@ -1,5 +1,6 @@
 from components.PlayWrightAuto.essencial import PlayEssencial
 from datetime import datetime, timezone
+import re
 
 
 class Twitter_Automation(PlayEssencial):
@@ -7,69 +8,112 @@ class Twitter_Automation(PlayEssencial):
         self.account = account
         super().__init__(f"https://www.x.com/{self.account}", playwright, browser_data_path, chrome_executable_path, browser, page)
 
-    def get_href(self, start_date :  datetime, end_date : datetime):
+    def collect_filtered_post_links(self, start_date :  datetime, end_date : datetime)-> list[dict]:
+        """
+            Collects links and post data published between two dates on an X (formerly Twitter) page.
+
+            This method scrolls through the user's timeline to dynamically load posts. 
+            For each post found within the specified date range, it extracts:
+            - post URL
+            - tweet text (with whitespace cleaned)
+            - publication date
+            - engagement metrics: replies, reposts, likes, and views
+
+            Parameters:
+            ----------
+            data_inicio : datetime
+                The minimum post date to include.
+            data_fim : datetime
+                The maximum post date to include.
+
+            Returns:
+            -------
+            List[Dict[str, Dict]]
+                A list of dictionaries where each item has the post URL as the key, 
+                and the associated metadata as the value.
+        """
         if not self.page:
             raise Exception("Browser or page not initialized. Call start_browser() first.")
-        
         self.set_url(self.current_url)
         self.page.goto(self.current_url, timeout=50000)
         self.page.wait_for_load_state('domcontentloaded', timeout=50000)
         self.page.wait_for_selector("//section[@class='css-175oi2r']", timeout=30000)
         self.page.wait_for_timeout(5000)
-        feed = self.page.locator('//div[@class="css-175oi2r r-150rngu r-16y2uox r-1wbh5a2 r-rthrr5"]')
-        count = feed.count()
-        print(f"Total de posts encontrados: {count}")
-        links_filtrados = []
-        running = True
-        while running:
-            self.page.mouse.wheel(0, 300)
-            self.page.wait_for_timeout(1000)
-            posts = feed.locator("//article")
-            count = posts.count()
-            if count == 0:
+        feed_container = self.page.locator('//div[@class="css-175oi2r r-150rngu r-16y2uox r-1wbh5a2 r-rthrr5"]')
+        total_posts = feed_container.count()
+        processed_hrefs = set()
+        filtered_posts = []
+        continue_collecting = True
+        while continue_collecting:
+            self.page.mouse.wheel(0, 1000)
+            self.page.wait_for_timeout(500)
+            posts = feed_container.locator("//article")
+            total_posts = posts.count()
+            if total_posts == 0:
                 print('Saindo do loop')
                 break
-            # print(f"Total de posts encontrados loop: {count}")
-            last_post = posts.nth(count -1)
 
-            # print("last_post is", last_post)
-            access_date = last_post.locator('//a[@class="css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-xoduu5 r-1q142lx r-1w6e6rj r-9aw3ui r-3s2u2q r-1loqt21"]')
-            post_datetime = access_date.locator("time").get_attribute("datetime")
-            post_datetime = datetime.strptime(post_datetime, "%Y-%m-%dT%H:%M:%S.%fZ")
-            print(post_datetime)
-
-            for i in range(count):
+            for i in range(total_posts):
                 post = posts.nth(i)
-                access_metrics = post.locator("//div[@class='css-175oi2r r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu']//div[@class='css-175oi2r r-18u37iz r-1h0z5md r-13awgt0']").all_inner_texts()
-                access_desc = post.locator("//div[@class='css-175oi2r r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu']//div[@class='css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-bnwqim']")
-                all_desc = access_desc.all()
-                post_descs = "".join([desc.inner_text().replace("\n", "") for desc in all_desc])
-                # access_date = post.locator('//a[@class="css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-xoduu5 r-1q142lx r-1w6e6rj r-9aw3ui r-3s2u2q r-1loqt21"]')
+                post.locator("//div[@class='css-175oi2r r-1kbdv8c r-18u37iz r-1wtj0ep r-1ye8kvj r-1s2bzr4']").first.wait_for(state="visible", timeout=5000)
+                engagement_summary_str = post.locator("//div[@class='css-175oi2r r-1kbdv8c r-18u37iz r-1wtj0ep r-1ye8kvj r-1s2bzr4']").get_attribute("aria-label")
+
+                datetime_element = post.locator('a:has(time)').first
+                datetime_element.wait_for(state="visible", timeout=5000)
+
+                datetime_str = datetime_element.locator("time").get_attribute("datetime")
+                post_datetime = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+                post_url = datetime_element.get_attribute("href")
+
+                if post_url in processed_hrefs:
+                    continue
+                processed_hrefs.add(post_url
+                                    )
+                def convert_text_to_metrics(metrics_text : str)-> dict:
+                    pattern = r"(\d+)\s+(respostas?|repost|curtidas?|visualizações)"
+                    matches = re.findall(pattern, metrics_text)
+                    metrics_dict = {}
+                    for value, key in matches:
+                        if not key.endswith('s'):
+                            key += 's'
+                        key = key.lower()
+                        metrics_dict[key] = value
+                    return metrics_dict
+                
+
+                post_metrics = convert_text_to_metrics(engagement_summary_str)
+                post_decription = post.locator('[data-testid="tweetText"]').inner_text()
+                post_decription = re.sub(r'\s+', ' ', post_decription).strip()
+
                 if post_datetime < start_date:
-                    running = False
+                    continue_collecting = False
                     print(f"Post de {post_datetime} está antes de {start_date}. Encerrando busca.")
                     break
                 if post_datetime >  end_date:
                     continue
-                get_href = access_date.get_attribute("href")
-                if get_href in [list(link.keys())[0] for link in links_filtrados]:
-                    print("ja existe")
-                    continue
-                else:
-                    if start_date <= post_datetime <= end_date:
-                        links_filtrados.append(({get_href : 
-                                                    {'Descrição' : post_descs, 
-                                                    'Data' : post_datetime.strftime("%d/%m/%Y %H:%M:%S"), 
-                                                    'Comentários' : access_metrics[0] if access_metrics[0] != '' else 0 , 
-                                                    'Compartilhamentos' :  access_metrics[1] if access_metrics[1] != '' else 0, 
-                                                    'Curtidas' :  access_metrics[2] if access_metrics[2] != '' else 0, 
-                                                    'Visualizações' :  access_metrics[3] if access_metrics[3] != '' else 0}}))
-                        print(f"Link adicionado: {get_href}")
-        print("Links filtrados:", links_filtrados)
-        return links_filtrados
+
+                print(f"Data do post: {post_datetime}")
+                print(f"Link encontrado: {post_url}")
+                print(f"Descrição do post: {post_decription}")
+                print(f"Métricas do post: {engagement_summary_str}")
+                print(f"Métricas convertidas: {post_metrics}")
+
+                if start_date <= post_datetime <= end_date:
+                    filtered_posts.append(({f"https://www.x.com{post_url}" : 
+                                                {'Descrição' : post_decription, 
+                                                'Data' : post_datetime, 
+                                                'Comentários' : post_metrics.get('respostas', '0'), 
+                                                'Compartilhamentos' : post_metrics.get('reposts', '0'),
+                                                'Curtidas' :  post_metrics.get('curtidas', '0'),
+                                                'Visualizações' :  post_metrics.get('visualizações', '0') }}))
+                    print(f"Link adicionado: {post_url}")
+        print("Links filtrados:", filtered_posts)
+        print(f"Total de links filtrados: {len(filtered_posts)}")
+        return filtered_posts
 
     def standard_procedure(self, dates: list[datetime]):
         self.start_browser_user()
-        data = self.get_href(dates[0], dates[1])
+        data = self.collect_filtered_post_links(dates[0], dates[1])
         self.stop_browser()
         return data
